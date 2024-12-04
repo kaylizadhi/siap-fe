4950946c62a533477abd1006dc955291ab0057f7
Mathilda Dellanova <mathilda.dellanova@ui.ac.id>
2024-12-04 20:29:23 +0700

    fix buat-survei

--- my-app/src/app/survei/buat-survei/page.js
+++ my-app/src/app/survei/buat-survei/page.js
@@ -128,32 +128,132 @@ export default function BuatSurvei() {
     }
   };
 
-    return (
-        <div>
-      		<b className={styles.headingSurvei}>Daftar Survei</b>  
-			<div className={styles.containerSurvei}>
-				<b className={styles.textFieldTitleSurvei}>Judul Survei</b>
-					<input className={styles.textFieldSurvei} type='text' name='judulsurvei' placeholder='Masukkan judul survei' onChange={(event) => setSurvei({...survei, nama_survei: event.target.value})}></input>
-				<b className={styles.textFieldTitleSurvei}>Nama Klien</b>
-					<input className={styles.textFieldSurvei} type='text' name='namaklien' placeholder='Masukkan nama klien' onChange={(event) => setSurvei({...survei, klien_id: event.target.value})}></input>
-				<b className={styles.textFieldTitleSurvei}>Daerah</b>
-					<input className={styles.textFieldSurvei} type='text' name='wilayahsurvei' placeholder='Masukkan wilayah survei' onChange={(event) => setSurvei({...survei, ruang_lingkup_survei: event.target.value})}></input>
-				<b className={styles.textFieldTitleSurvei}>Harga</b>
-					<input className={styles.textFieldSurvei} type='text' name='hargasurvei' placeholder='Masukkan harga' onChange={(event) => setSurvei({...survei, harga_survei: event.target.value})}></input>
-				<b className={styles.textFieldTitleSurvei}>Jumlah Responden</b>
-					<input className={styles.textFieldSurvei} type='text' name='jumlahresponden' placeholder='Masukkan jumlah responden' onChange={(event) => setSurvei({...survei, jumlah_responden: event.target.value})}></input>
-				<b className={styles.textFieldTitleSurvei}>Tanggal Mulai</b>
-					<input min={new Date().toISOString().split("T")[0]} className={styles.textFieldSurvei} type='date' name='tanggalmulai' placeholder='Masukkan tanggal mulai survei'onChange={(event) => setSurvei({...survei, waktu_mulai_survei: event.target.value})}></input>
-				<b className={styles.textFieldTitleSurvei}>Tanggal Berakhir</b>
-					<input min={new Date().toISOString().split("T")[0]} className={styles.textFieldSurvei} type='date' name='tanggalberakhir' placeholder='Masukkan tanggal berakhir survei'onChange={(event) => setSurvei({...survei, waktu_berakhir_survei: event.target.value})}></input>
-				<b className={styles.textFieldTitleSurvei}>Tipe Survei</b>
-					<select className={styles.textFieldSurvei} type='text' name='tipesurvei' onChange={(event) => setSurvei({...survei, tipe_survei: event.target.value})}>
-						<option value="Paper-based">Paper-based</option>
-						<option value="Digital">Digital</option>
-						<option value="Paper-based">Lainnya</option>
-					</select>
-			</div>			
-			<button onClick={() => HandleSubmit()} className={styles.primaryButtonSurvei}>Simpan</button>	
-			<button className={styles.secondaryButtonSurvei}>Batal</button>
-    	</div>);
-};
+  return (
+    <div>
+		<b className={styles.headingSurvei}>Daftar Survei</b>
+		<form onSubmit={handleSubmit} className={styles.containerSurvei}>
+			<b className={styles.textFieldTitleSurvei}>Nama Survei</b>
+			<input
+				className={styles.textFieldSurvei}
+				type="text"
+				placeholder="Masukkan judul survei"
+				onChange={(event) => setSurvei({ ...survei, nama_survei: event.target.value })}
+			/>
+
+        <b className={styles.textFieldTitleSurvei}>Nama Klien</b>
+		<select
+          	className={styles.textFieldSurveiDropdown}
+          	onChange={(event) => setSurvei({ ...survei, klien_id: event.target.value })}
+        >
+          	<option value="">Pilih Klien</option>
+          		{klien.map((e) => (
+            <option key={e.id} value={e.id}>
+              	{e.nama_klien}
+            </option>
+          	))}
+        </select>
+
+        <b className={styles.textFieldTitleSurvei}>Ruang Lingkup Survei</b>
+        <div className={styles.radioGroupSurvei}>
+          	{["Nasional", "Provinsi", "Kota"].map((scope) => (
+            <label key={scope}>
+              	<input
+                	type="radio"
+                	name="ruanglingkup"
+                	value={scope}
+                	onChange={handleRuangLingkupChange}/>
+              		{scope}
+            </label>))}
+        </div>
+
+        <b className={styles.textFieldTitleSurvei}>Daerah</b>
+        <ul className={styles.daerahList}>
+          	{wilayahSurvei.map((daerah) => (
+            <li key={daerah.id} className={styles.daerahList}>
+            {daerah.name}
+            <button
+                style={{ marginLeft: "10px" }}
+                onClick={() => removeDaerah(daerah.id)}>
+                Hapus
+            </button>
+            </li>
+          ))}
+        </ul>
+
+        <select id="daerahSelect" className={styles.textFieldSurveiDropdown}>
+          <option value="">Pilih Daerah</option>
+          {daerahOptions.map((daerah) => (
+            <option key={daerah.id} value={daerah.id}>
+              {daerah.name}
+            </option>
+          ))}
+        </select>
+		<PlusIcon 
+			className={styles.iconEdit} 
+			onClick={addDaerah}
+		/>
+
+        <b className={styles.textFieldTitleSurvei}>Harga</b>
+        <input
+          className={styles.textFieldSurvei}
+          type="number"
+          placeholder="Masukkan harga"
+          onChange={(event) =>
+            setSurvei({ ...survei, harga_survei: event.target.value })
+          }
+        />
+        <b className={styles.textFieldTitleSurvei}>Jumlah Responden</b>
+        <input
+          className={styles.textFieldSurvei}
+          type="number"
+          placeholder="Masukkan jumlah responden"
+          onChange={(event) =>
+            setSurvei({ ...survei, jumlah_responden: event.target.value })
+          }
+        />
+        <b className={styles.textFieldTitleSurvei}>Tanggal Mulai</b>
+        <input
+          min={new Date().toISOString().split("T")[0]}
+          className={styles.textFieldSurvei}
+          type="date"
+          onChange={(event) =>
+            setSurvei({ ...survei, waktu_mulai_survei: event.target.value })
+          }
+        />
+        <b className={styles.textFieldTitleSurvei}>Tanggal Berakhir</b>
+        <input
+          min={new Date().toISOString().split("T")[0]}
+          className={styles.textFieldSurvei}
+          type="date"
+          onChange={(event) =>
+            setSurvei({ ...survei, waktu_berakhir_survei: event.target.value })
+          }
+        />
+        <b className={styles.textFieldTitleSurvei}>Tipe Survei</b>
+        <select
+          className={styles.textFieldSurveiDropdown}
+          onChange={(event) =>
+            setSurvei({ ...survei, tipe_survei: event.target.value })
+          }
+        >
+          <option value="Paper-based">Paper-based</option>
+          <option value="Digital">Digital</option>
+          <option value="Lainnya">Lainnya</option>
+        </select>
+      
+
+		<button 
+			type="submit" 
+			className={styles.primaryButtonSurvei}>
+				Simpan
+		</button>
+		<button 
+			type="button" 
+			onClick={handleBackToSurvei}
+			className={styles.secondaryButtonSurvei}>
+				Batal
+		</button>
+	  </form>
+    </div>
+  );
+}
