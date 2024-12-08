1807d4c084ae5e154288b0ae7cb807549f9262ee
Kay Lizadhi <khairinka.rania01@ui.ac.id>
2024-12-08 21:37:45 +0700

    config eslint

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+// module.exports = {
+//     eslint: {
+//       ignoreDuringBuilds: true,
+//     },
+//   };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, // This will ignore ESLint errors during the build
+    },
+  };
+  
+  module.exports = nextConfig;
+  
